c316ba89cd8276d189c090d8b9207657317b8804
Luis Augusto Melo Rohten <luisaugustomelo@gmail.com>
2017-08-24 01:48:31 -0300

    first commit - init project

---
+++ index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql')
+
+//modificar a conexão
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'crowdsub'
+});
+
+connection.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected ... nn");
+    } else {
+        console.log("Error connecting database ... nn");
+    }
+});
+
+//1) pegar todos os resultados da agregação do banco de dados
+
+//2) gerar um srt com os resultados
+
